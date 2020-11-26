ca9343e1b5cd7f9c09ecb2280d292abe9370d08a
GilbramAlifMiftahulFuan <fuanguns@gmail.com>
2020-11-26 15:42:11 +0700

    Initial commit

---
+++ app/src/main/java/com/gilbram/newsapi/DetailActivity.kt
@@ -0,0 +1,48 @@
+package com.gilbram.newsapi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import coil.api.load
+import coil.size.Scale
+import com.gilbram.newsapi.Model.ArticlesItem
+import com.gilbram.newsapi.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+
+    companion object{
+        const val DETAIL_NEWS = "Detail_News"
+    }
+
+    private lateinit var binding: ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+//        setContentView(R.layout.activity_main)
+
+        val data = intent.getParcelableExtra(DETAIL_NEWS) as ArticlesItem
+
+        binding.run {
+            setContentView(root)
+            setSupportActionBar(toolBar)
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+            title= data.title
+
+            imgToolbar.apply {
+                load(data.urlToImage){
+                    scale(Scale.FILL)
+                }
+                contentDescription = data.description
+            }
+            txtContent.text= data .content
+            txtDate.text = data.publishedAt
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        finish()
+        return true
+    }
+}
---
+++ app/src/main/java/com/gilbram/newsapi/Service/RetrofitBuilder.kt
@@ -0,0 +1,28 @@
+package com.gilbram.newsapi.Service
+
+
+import com.gilbram.newsapi.Model.ResponseNews
+import okhttp3.OkHttpClient
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Headers
+
+object RetrofitBuilder {
+    private val client = OkHttpClient.Builder().build()
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("https://newsapi.org/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(client)
+        .build()
+
+    fun getService() = retrofit.create(TopHeadLine::class.java)
+
+}
+//kesalahan spasi pada authorization//
+interface TopHeadLine{
+    @Headers("Authorization: 730b49091db74e40b16924b153a77032")
+    @GET("/v2/top-headlines?country=id")
+    fun fetchHeadlines (): Call<ResponseNews>
+}
